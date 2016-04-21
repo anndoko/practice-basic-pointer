fe98a1fe8dfd6f815765d08180ccb920cd540d95
Anndo <handcuffanndo@gmail.com>
2016-04-22 02:20:56 +0800

    Add comments

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@ using namespace std;
 int main(){
 
     int x = 10; // variable x
-    int *p;
-    p = &x;
+    int *p; // pointer variable
+    p = &x; // store the address of x in pointer variable
 
+    // x
     cout << "Value of x variable: \n" << x << "\n" << endl;
     
+    // &x
     cout << "Address of x variable: " << &x << "\n" << endl;
 
+    // *p
     cout << "Access the variable at the address: \n" << *p << "\n" << endl;
     
+    // p
     cout << "Address stored in p pointer variable: \n" << p << "\n" << endl;
 
     return 0;
